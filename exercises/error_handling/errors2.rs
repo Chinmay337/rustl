5040866ed182a8d6824aafe392455094d4d3073b
Chinmay B <cb.kalz@gmail.com>
2023-05-19 23:20:43 -0400

    generics

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -22,11 +22,15 @@
 use std::num::ParseIntError;
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
-    let processing_fee = 1;
-    let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+    match item_quantity.parse::<i32>() {
+        Ok(qty) => Ok(qty * 5 + 1),
+        Err(e) => Err(e),
+    }
+    // let processing_fee = 1;
+    // let cost_per_item = 5;
+    // let qty = item_quantity.parse::<i32>();
 
-    Ok(qty * cost_per_item + processing_fee)
+    // Ok(qty * cost_per_item + processing_fee)
 }
 
 #[cfg(test)]
